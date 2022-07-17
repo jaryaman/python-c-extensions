66440855286e2f7417f64ec450fe21a755c8476f
Juvid Aryaman <j.aryaman25@gmail.com>
2022-07-17 15:15:09 +0100

    feat: arrays (#2)

--- 01-c-extension-module/pysample.c
+++ 01-c-extension-module/pysample.c
@@ -47,12 +47,49 @@ static PyObject *py_multiply(PyObject *self, PyObject *args) {
     return Py_BuildValue("i", ab);
 }
 
+/* Call double avg(double *, int) */
+static PyObject *py_avg(PyObject *self, PyObject *args) {
+    PyObject *bufobj;
+    Py_buffer view;
+    double result;
+    /* Get the passed Python object */
+    if (!PyArg_ParseTuple(args, "O", &bufobj)) {
+        return NULL;
+    }
+
+    /* Attempt to extract buffer information from it */
+    if (PyObject_GetBuffer(bufobj, &view, PyBUF_ANY_CONTIGUOUS | PyBUF_FORMAT) == -1) {
+        return NULL;
+    }
+
+    if (view.ndim != 1){
+        PyErr_SetString(PyExc_TypeError, "Expected a 1-dimensional array");
+        PyBuffer_Release(&view);
+        return NULL;
+    }
+
+    /* Check the type of items in the array */
+    if (strcmp(view.format, "d") != 0) {
+        PyErr_SetString(PyExc_TypeError, "Expected an array of doubles");
+        PyBuffer_Release(&view);
+        return NULL;
+    }
+
+    /* Pass the raw buffer and size to the C function */
+    result = avg(view.buf, view.shape[0]);
+
+    /* Indicate we're done working with the buffer */
+    PyBuffer_Release(&view);
+    return Py_BuildValue("d", result);
+}
+
 /* Module method table */
 static PyMethodDef SampleMethods[] = {
     {"gcd", py_gcd, METH_VARARGS, "Greatest common divisor"},
     {"in_mandel", py_in_mandel, METH_VARARGS, "Mandelbrot test"},
     {"divide", py_divide, METH_VARARGS, "Integer division"},
     {"multiply", py_multiply, METH_VARARGS, "Integer multiplication"},
+    {"avg", py_avg, METH_VARARGS, "Double array averaging"},
     {NULL, NULL, 0, NULL}
 };
 
--- 01-c-extension-module/test.py
+++ 01-c-extension-module/test.py
@@ -1,5 +1,6 @@
 import sample
-
+import numpy as np
+import array
 
 def main():
     print(sample.gcd(35, 42))
@@ -7,6 +8,8 @@ def main():
     print(sample.in_mandel(2.0, 1.0, 500))
     print(sample.divide(42, 8))
     print(sample.multiply(6, 7))
+    print(sample.avg(np.array([4.1, 4.2, 4.3])))
+    print(sample.avg(array.array('d', [6.65, 6.66, 6.67])))
 
 
 if __name__ == '__main__':
