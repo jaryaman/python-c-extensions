1c1ff61ba0dc18fae7d1f8354f8cd25b8391ce49
jaryaman <j.aryaman25@gmail.com>
2022-07-23 16:26:23 +0100

    feat: capsules

---
+++ 02-capsules/setup_ptexample.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+setup(name='ptexample',
+      ext_modules=[
+          Extension('ptexample',
+                    ['ptexample.c'],
+                    include_dirs=[],  # May need pysample.h directory
+                    )
+      ]
+      )
